396677557aa4815884f878117a28bbe961534aed
Mohammadbalata <balatam437@gmail.com>
2025-11-29 19:04:43 +0200

    adding live url

--- src/keep-alive.service.ts
+++ src/keep-alive.service.ts
@@ -12,7 +12,7 @@ export class KeepAliveService {
   // Runs every 10 minutes
   @Cron(CronExpression.EVERY_10_MINUTES)
   async pingServer() {
-    const url = `https://backend-6adn.onrender.com/keep-alive`; 
+    const url = `https://backend-5549.onrender.com/keep-alive`; 
     try {
       const res = await firstValueFrom(this.http.get(url));
       this.logger.log(`Pinged ${url}, data: ${res.data.data}`);
