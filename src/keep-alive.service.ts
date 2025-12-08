fd3e01c57b368cedf59265ed3b96c469941ff0b4
Mohammadbalata <balatam437@gmail.com>
2025-12-08 18:39:33 +0200

    fix

--- src/keep-alive.service.ts
+++ src/keep-alive.service.ts
@@ -10,12 +10,15 @@ export class KeepAliveService {
   constructor(private readonly http: HttpService) {}
 
   // Runs every 10 minutes
-  @Cron(CronExpression.EVERY_5_MINUTES)
+  @Cron(CronExpression.EVERY_MINUTE)
   async pingServer() {
     const url = `https://backend-6adn.onrender.com/keep-alive`;
+    const frontUrl = "https://gazadamageassessments.onrender.com";
     try {
       const res = await firstValueFrom(this.http.get(url));
+      const front = await firstValueFrom(this.http.get(frontUrl));
       this.logger.log(`Pinged ${url}, data: ${res.data.data}`);
+      this.logger.log(`Pinged ${front}, data: ${front.data}`);
     } catch (err) {
       this.logger.error(`Failed to ping ${url}: ${err.message}`);
     }
